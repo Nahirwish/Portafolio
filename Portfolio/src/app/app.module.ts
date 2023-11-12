2fcb560800baaeaaa35fd09f154835d704cad49b
Nahir Cavallero <nahir.afk@gmail.com>
2023-11-12 19:32:41 -0300

    Instalacion de bootstrap y solucion de problemas de compatibilidad

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { FooterComponent } from './components/nav/footer/footer.component';
-import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    NavComponent
+    NavBarComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
