2cd991b43e890bdacf0a5308b59d3ff429f14952
Nahir Cavallero <nahir.afk@gmail.com>
2024-01-16 20:39:12 -0300

    primeros routes funcionando

--- Portfolio/src/app/app-routing/app-routing.module.ts
+++ Portfolio/src/app/app-routing/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from '../home/home.component';
+import { HomeComponent } from '../components/home/home.component';
 import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideRouter } from '@angular/router';
 import { routes } from './app-routing/app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LogoComponent } from './components/logo/logo.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -30,7 +31,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     NgbModule,
     RouterModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   
   providers: [provideRouter(routes)],
