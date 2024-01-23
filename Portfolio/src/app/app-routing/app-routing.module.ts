fe8db4c59e8d26b39162eb895c810430e6bc7167
Nahir Cavallero <nahir.afk@gmail.com>
2024-01-23 14:11:01 -0300

    certificados agregados

--- Portfolio/src/app/app-routing/app-routing.module.ts
+++ Portfolio/src/app/app-routing/app-routing.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from '../components/home/home.component';
 import { ContactComponent } from '../contact/contact.component';
+import { PortfolioComponent } from '../components/portfolio/portfolio.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     //{path: '**', component: PageNotFoundComponent},
     {path: 'home', component: HomeComponent},
-    {path: 'contact', component: ContactComponent}
+    {path: 'contact', component: ContactComponent},
+    {path: 'portfolio', component: PortfolioComponent}
 ];
 
 
--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LogoComponent } from './components/logo/logo.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     LogoComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    PortfolioComponent
     
   ],
   imports: [
